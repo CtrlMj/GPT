bd8a274cdd6f0e0352a5d7f825038ba50fcb2d47
CtrlMj <alikhanimjd@gmail.com>
2024-12-02 19:53:57 -0500

    add training configs as arguments

--- train_gpt.py
+++ train_gpt.py
@@ -8,31 +8,28 @@ from torch.nn import functional as F
 from torch.nn.parallel import DistributedDataParallel
 
 from ray.data.preprocessor import Preprocessor
-from ray.air.config import CheckpointConfig, DatasetConfig, RunConfig, ScalingConfig
+from ray.train import CheckpointConfig, DatasetConfig, RunConfig, ScalingConfig
 import ray.train as raytrain
 from ray.train import Checkpoint, session
 from ray.train.torch import TorchCheckpoint, TorchTrainer
 
 from train_gpt import GPT
+from typing_extensions import Annotated
+from utils import get_batch, read_data
 
 # hyper params
 torch.manual_seed(1337)
-context_size = 8
-batch_size = 64
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu' # change position embedding's device if cuda is available
-eval_iters = 200
-n_embed = 32
-n_head = 4
-dropout = 0.2
 
-def train_step(model, train_data, batch_size, optimizer, n_steps):
+
+
+
+app = typer.Typer()
+
+def train_step(model, context_size, batch_size, optimizer, n_steps):
     model.train()
     total_loss = 0
     for step in range(n_steps):
-        xb, yb = get_batch(train_data, split='train')
+        xb, yb = get_batch(context_size, batch_size, split='train')
         xb.to(device)
         yb.to(device)
         logits, loss = model(xb, yb)
@@ -44,12 +41,12 @@ def train_step(model, train_data, batch_size, optimizer, n_steps):
 
 
 @torch.no_grad()
-def eval_step(model, val_data, batch_size, n_steps):
+def eval_step(model, context_size, batch_size, n_steps):
     model.eval()
     total_loss = 0
     with torch.inference_mode():
         for step in range(n_steps):
-            xb, yb = get_batch(val_data, split='eval')
+            xb, yb = get_batch(context_size, batch_size, split='eval')
             xb.to(device)
             yb.to(device)
             logits, loss = model(xb, yb)
@@ -59,8 +56,9 @@ def eval_step(model, val_data, batch_size, n_steps):
 
 # Training loop
 def train_loop_per_worker(config):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' # change position embedding's device if cuda is available
     # Hyperparameters
-    dropout_p = config["dropout_p"]
+    dropout = config["dropout"]
     lr = config["lr"]
     num_epochs = config["num_epochs"]
     batch_size = config["batch_size"]
@@ -68,12 +66,12 @@ def train_loop_per_worker(config):
     n_eval_steps = config["n_eval_steps"]
     n_heads = config["n_heads"]
     n_blocks = config["n_blocks"]
+    context_size = config["context_size"]
 
     # Get datasets
-    torch.manual_seed(1337) #set seed
+    torch.manual_seed(1337)  # set seed
     train_data, val_data, encode, decode, vocab_size = read_data()
    
-
     # Model 
     gpt = GPT(n_heads, n_blocks, vocab_size)
     gpt = raytrain.torch.prepare_model(gpt)
@@ -85,8 +83,8 @@ def train_loop_per_worker(config):
     batch_size_per_worker = batch_size // raytrain.get_context().get_world_size()
     for epoch in range(num_epochs):
         # Step
-        train_loss = train_step(gpt, train_data, batch_size_per_worker, optimizer, n_train_steps)
-        val_loss = eval_step(gpt, val_data, batch_size_per_worker, n_eval_steps)
+        train_loss = train_step(gpt, context_size, batch_size_per_worker, optimizer, n_train_steps)
+        val_loss = eval_step(gpt, context_size, batch_size_per_worker, n_eval_steps)
 
         # Checkpoint
         base_model = gpt.module if isinstance(gpt, DistributedDataParallel) else gpt
@@ -102,7 +100,32 @@ def train_loop_per_worker(config):
                         checkpoint=checkpoint)
 
 
-def train_gpt():
+
+@app.command()
+def train_gpt(
+    dropout: Annotated[float, typer.Option(help="Dropout porbability")] = 0.2,
+    n_heads: Annotated[int, typer.Option(help="number of attention heads")] = 4,
+    n_blocks: Annotated[int, typer.Option(help="number of decoder blocks")] = 3,
+    n_embed: Annotated[int, typer.Option(help="token embedding dimentionality")] = 32,
+    eval_iters: Annotated[int, typer.Option(help="Number of iterations for evaluation")] = 200,
+    lr: Annotated[float, typer.Option(help="Optimization learning rate")] = 3e-4,
+    context_size: Annotated[int, typer.Option(help="Context window size")] = 8,
+    batch_size: Annotated[int, typer.Option(help="batch size")] = 64,
+    num_epochs: Annotated[int, typer.Option(help="number of epochs")] = 3,
+    n_train_steps: Annotated[int, typer.Option(help="number of training steps per epoch")] = 5000,
+    n_eval_steps: Annotated[int, typer.Option(help="number of of eval steps per epoch")] = 100
+):
+    train_config = {
+        'dropout': dropout,
+        'lr': lr,
+        'num_epochs': num_epochs,
+        'batch_size': batch_size,
+        'n_train_steps': n_train_steps,
+        'n_eval_steps': n_eval_steps,
+        'n_heads': n_heads,
+        'n_blocks': n_blocks,
+        'context_size': context_size,
+    }
     scaling_config = ScalingConfig(
         num_workers=6,
         )
@@ -114,10 +137,13 @@ def train_gpt():
                            storage_path=os.path.abspath("./ray_results"))
     
     trainer = TorchTrainer(
-    train_loop_per_worker=train_loop_per_worker,
-    train_loop_config=train_config,
-    scaling_config=scaling_config,
-    run_config=run_config,
+        train_loop_per_worker=train_loop_per_worker,
+        train_loop_config=train_config,
+        scaling_config=scaling_config,
+        run_config=run_config,
     )
+
     results = trainer.fit()
+
+    return results.best_checkpoints[0].path
     
